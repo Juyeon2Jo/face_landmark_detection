6a3422c12a822577c5c382a4a831874d4a43e331
place0 <thdud030101@naver.com>
2023-11-15 11:12:36 +0900

    모델 수정

--- csv_to_input.py
+++ csv_to_input.py
@@ -14,14 +14,27 @@ data.dropna(subset=[data.columns[0]], inplace=True)
 
 # 정규화
 scaler = StandardScaler()
-scaled_data = scaler.fit_transform(data)
 
-# 결과 출력
-print(data.head())
-# 결과 확인
-print(f"Original number of features: {data.shape[1]}")
-print(f"Reduced number of features: {scaled_data.shape[1]}")
+# X, Y, Z 좌표 분리
+x_columns = data.columns[1:469]  # 첫 번째 열은 이미지 이름이므로 제외
+y_columns = data.columns[469:937]
+z_columns = data.columns[937:1405]
 
-# 변환된 주성분 데이터 저장
-with open('principal_components.pkl', 'wb') as file:
-    pickle.dump(scaled_data, file)
+x_data = data[x_columns]
+y_data = data[y_columns]
+z_data = data[z_columns]
+
+# 정규화 적용
+x_scaled = scaler.fit_transform(x_data)
+y_scaled = scaler.fit_transform(y_data)
+z_scaled = scaler.fit_transform(z_data)
+
+# 각 좌표별로 파일 저장
+with open('x_principal_components.pkl', 'wb') as file:
+    pickle.dump(x_scaled, file)
+
+with open('y_principal_components.pkl', 'wb') as file:
+    pickle.dump(y_scaled, file)
+
+with open('z_principal_components.pkl', 'wb') as file:
+    pickle.dump(z_scaled, file)
--- landmark_AutoEncoder.py
+++ landmark_AutoEncoder.py
@@ -1,26 +1,58 @@
-from keras.layers import Input, Dense
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Dense, Lambda, Reshape, Conv2DTranspose
 from keras.models import Model
+import tensorflow as tf
+import numpy as np
 import pickle
 
-input_dim = 468 * 3  # 입력 차원
-# 변환된 데이터 불러오기
-with open('principal_components.pkl', 'rb') as file:
-    X_train = pickle.load(file)
+# x, y, z 데이터 불러오기
+with open('x_principal_components.pkl', 'rb') as file:
+    x_data = pickle.load(file)
 
-# Autoencoder 모델 구성
-input_layer = Input(shape=(input_dim,))
-encoded = Dense(256, activation='relu')(input_layer)
-encoded = Dense(128, activation='relu')(encoded)
-decoded = Dense(256, activation='relu')(encoded)
-decoded = Dense(input_dim, activation='sigmoid')(decoded)
-autoencoder = Model(input_layer, decoded)
+with open('y_principal_components.pkl', 'rb') as file:
+    y_data = pickle.load(file)
 
-# 컴파일
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+with open('z_principal_components.pkl', 'rb') as file:
+    z_data = pickle.load(file)
+x_train = np.stack((x_data, y_data, z_data), axis=-1)
 
-# 모델 학습
-autoencoder.fit(X_train, X_train,  # 입력과 출력이 동일
-                epochs=50,        # 에폭 수
-                batch_size=256,   # 배치 크기
-                shuffle=True,     # 데이터 셔플
-                validation_split=0.2)  # 검증 데이터 비율
+print(x_train.shape)  # 형태 확인
+
+input_dim = (468, 3, 1)
+encoder_input = Input(shape=input_dim)
+
+# 인코더 구조
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoder_input)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Flatten()(x)
+encoded = Dense(128, activation='relu')(x)
+
+# 디코더 구조
+x = Dense(468 * 3 * 1)(encoded)  # 중간 차원을 크게 설정
+x = Reshape((468, 3, 1))(x)  # 3차원 형태로 변환
+
+# Conv2DTranspose 레이어를 조정하여 원하는 출력 크기를 얻습니다.
+x = Conv2DTranspose(16, (3, 3), strides=(1, 1), activation='relu', padding='same')(x)  # 스트라이드 조정
+x = Conv2DTranspose(32, (3, 3), strides=(1, 1), activation='relu', padding='same')(x)  # 스트라이드 조정
+decoder_output = Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same')(x)  # 채널 수를 3으로 변경
+
+auto_encoder = Model(encoder_input, decoder_output)
+auto_encoder.compile(optimizer='adam', loss='mean_squared_error')
+
+# x_train 형태 조정
+x_train = np.expand_dims(x_train, axis=-1)
+
+# 훈련 설정
+batch_size = 32
+epochs = 50
+validation_split = 0.2  # 검증 데이터로 사용할 비율 (예: 20%)
+
+# 모델 훈련
+auto_encoder.fit(
+    x_train, x_train,  # 입력 데이터와 레이블(여기서는 동일)
+    batch_size=batch_size,
+    epochs=epochs,
+    validation_split=validation_split
+)
+
